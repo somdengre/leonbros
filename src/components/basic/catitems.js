77222b4bd6ad76e6b1afd0b16bef1ebcac8f7147
Som Dengre <somdengre852979@gmail.com>
2023-07-07 17:38:43 +0530

    backend merged

--- src/components/basic/catitems.js
+++ src/components/basic/catitems.js
@@ -1,45 +1,90 @@
-import React from 'react'
-import Item from '../../components/basic/item'
-import item1 from '../../Assets/hubbearing_items/item1.svg'
-import item2 from '../../Assets/hubbearing_items/item2.svg'
-import item3 from '../../Assets/hubbearing_items/item3.svg'
-import item4 from '../../Assets/hubbearing_items/item4.svg'
-import './catitems.css';
+import React, { useEffect, useState } from "react";
+import Item from "../../components/basic/item";
+import item1 from "../../Assets/hubbearing_items/item1.svg";
+import item2 from "../../Assets/hubbearing_items/item2.svg";
+import item3 from "../../Assets/hubbearing_items/item3.svg";
+import item4 from "../../Assets/hubbearing_items/item4.svg";
+import "./catitems.css";
 function Catitems() {
-  return (
-    <div>
-        <div className='citems'>
-          <Item image = {item1} iname = 'Quick Steer (K6395)' ides = 'Control Arm Bushing' irat = '4.5' inum = '(36,177)' price = '$3.20'></Item>
-          <Item image = {item2} iname = 'MOOG (K202029)' ides = 'Control Arm Bushing' irat = '4.5' inum = '(70,199)' price = '$7.39'></Item>
-          <Item image = {item3} iname = 'MOOG (K80833)' ides = 'Control Arm Bushing' irat = '4.5' inum = '(70,199)' price = '$52.79'></Item>
-          <Item image = {item4} iname = 'MOOG (K80834)' ides = 'Control Arm Bushing' irat = '4.5' inum = '(40,128)' price = '$53.79'></Item>
-          
-        </div>
-        <div className='citems'>
-          <Item image = {item1} iname = 'Quick Steer (K6395)' ides = 'Control Arm Bushing' irat = '4.5' inum = '(36,177)' price = '$3.20'></Item>
-          <Item image = {item2} iname = 'MOOG (K202029)' ides = 'Control Arm Bushing' irat = '4.5' inum = '(70,199)' price = '$7.39'></Item>
-          <Item image = {item3} iname = 'MOOG (K80833)' ides = 'Control Arm Bushing' irat = '4.5' inum = '(70,199)' price = '$52.79'></Item>
-          <Item image = {item4} iname = 'MOOG (K80834)' ides = 'Control Arm Bushing' irat = '4.5' inum = '(40,128)' price = '$53.79'></Item>
-          
-        </div>
-        <div className='citems'>
-          <Item image = {item1} iname = 'Quick Steer (K6395)' ides = 'Control Arm Bushing' irat = '4.5' inum = '(36,177)' price = '$3.20'></Item>
-          <Item image = {item2} iname = 'MOOG (K202029)' ides = 'Control Arm Bushing' irat = '4.5' inum = '(70,199)' price = '$7.39'></Item>
-          <Item image = {item3} iname = 'MOOG (K80833)' ides = 'Control Arm Bushing' irat = '4.5' inum = '(70,199)' price = '$52.79'></Item>
-          <Item image = {item4} iname = 'MOOG (K80834)' ides = 'Control Arm Bushing' irat = '4.5' inum = '(40,128)' price = '$53.79'></Item>
-          
-        </div>
-        <div className='citems'>
-          <Item image = {item1} iname = 'Quick Steer (K6395)' ides = 'Control Arm Bushing' irat = '4.5' inum = '(36,177)' price = '$3.20'></Item>
-          <Item image = {item2} iname = 'MOOG (K202029)' ides = 'Control Arm Bushing' irat = '4.5' inum = '(70,199)' price = '$7.39'></Item>
-          <Item image = {item3} iname = 'MOOG (K80833)' ides = 'Control Arm Bushing' irat = '4.5' inum = '(70,199)' price = '$52.79'></Item>
-          <Item image = {item4} iname = 'MOOG (K80834)' ides = 'Control Arm Bushing' irat = '4.5' inum = '(40,128)' price = '$53.79'></Item>
-          
-        </div>
+  const [parts, setParts] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
+  let currIndex = 0;
 
-        <div><button className='catbtn'>Load More</button></div>
-    </div>
-  )
+  useEffect(() => {
+    try {
+      fetch("https://leonbros-backend.vercel.app/v1/items/getItems")
+        .then((res) => res.json())
+        .then((res) => {
+          setParts(res.data);
+          setLoading(false);
+        });
+    } catch (error) {
+      setLoading(false);
+      setError(true);
+    }
+  }, []);
+
+  return (
+    <>
+      {loading ? (
+        <div className="spinner-border" role="status">
+          <span className="sr-only">Loading...</span>
+        </div>
+      ) : error ? (
+        <span>Error Occured</span>
+      ) : (
+        <div>
+          {parts.map((_, index) => {
+            {/* return <></> */}
+            if (index === currIndex) {
+              let i = 0;
+              const temp = [];
+              while (i < 4 && currIndex < parts.length) {
+                const part = parts[currIndex];
+                if (
+                  part &&
+                  part.image &&
+                  part.moreInfo.length > 2 &&
+                  part.features
+                ) {
+                  i++;
+                  temp.push(part);
+                }
+                  currIndex++;
+              }
+              return (
+                <div className="citems">
+                  {temp.map((part) => {
+                    return (
+                      <Item
+                        image={part.image}
+                        iname={part.moreInfo.replace(
+                          "More Information for ",
+                          ""
+                        )}
+                        ides="Control Arm Bushing"
+                        irat="4.5"
+                        inum="(36,177)"
+                        price=" $3.20"
+                        data={part}
+                      />
+                    );
+                  })}
+                </div>
+              );
+            }
+            return <></>
+          })}
+
+
+          <div>
+            <button className="catbtn">Load More</button>
+          </div>
+        </div>
+      )}
+    </>
+  );
 }
 
-export default Catitems
+export default Catitems;
--- src/components/basic/item.js
+++ src/components/basic/item.js
@@ -1,11 +1,12 @@
-import React, {useState} from 'react'
-import "./item.css"
-import star from '../../Assets/hubbearing_items/star.svg'
-import item1 from '../../Assets/hubbearing_items/item1.svg'
-import cross from '../../Assets/cross.svg'
-import {Modal} from 'react-bootstrap';
+import React, { useState } from "react";
+import "./item.css";
+import star from "../../Assets/hubbearing_items/star.svg";
+import item1 from "../../Assets/hubbearing_items/item1.svg";
+import cross from "../../Assets/cross.svg";
+import { Modal } from "react-bootstrap";
 
 function MyVerticallyCenteredModal(props) {
+  const features = props.data.features.split("\n");
   return (
     <Modal
       {...props}
@@ -14,92 +15,104 @@ function MyVerticallyCenteredModal(props) {
       centered
     >
       <Modal.Body>
-        <div data-dismiss="modal" className='pcross' onClick={() => props.setShow(false)}>
+        <div
+          data-dismiss="modal"
+          className="pcross"
+          onClick={() => props.setShow(false)}
+        >
           <img className="picross" src={cross} alt="close" />
         </div>
-        <div className='pcontain'>
-            <div className='pimg'>
-                <img className="pimage" src={item1} alt="hub bearing" />
+        <div className="pcontain">
+          <div className="pimg">
+            <img className="pimage" src={props.data.image} alt="hub bearing" />
+          </div>
+          <div className="pinfo">
+            <div className="phead">
+              {props.data.moreInfo.replace("More Information for ", "")}
             </div>
-            <div className='pinfo'>
-                <div className='phead'>QUICK STEER K6395 Kit</div>
-                <div className='pcat'>Category: Control Arm Bushing</div>
-                <div className='prat'>
-                  <div className='istar'>
-                      <div className='istars'>4.5</div>
-                      <div className='star'>
-                          <img className="imgstar" src={star} alt="hub bearing" />
-                      </div>
-                  </div>
-                  <div className='prnum'>(36177)</div>
+            <div className="pcat">Category: Control Arm Bushing</div>
+            <div className="prat">
+              <div className="istar">
+                <div className="istars">4.5</div>
+                <div className="star">
+                  <img className="imgstar" src={star} alt="hub bearing" />
                 </div>
-                <div className='pcont'>Kit; Includes Forward & Rearward Bushing</div>
-                <div className='pfeat'>
-                    <div className='pfhead'>Features:</div>
-                    <div className='pcontent'>
-                        <ul>
-                            <li>High strength rubber</li>
-                            <li>Engineered to provide long life and dependability</li>
-                        </ul>
-                    </div>
-                </div>
-                <div className='pnums'>OEM/ Intercahnge Numbers: 15607586, 15993180</div>
-                <div className='pabt'>Closeout Inventory Warranty Information:Wholesaler Closeout items have a 30 day warranty (but still are covered by our Return Policy).</div>
-                <div className='pprice'>$3.20</div>
+              </div>
+              <div className="prnum">(36177)</div>
             </div>
-
+            <div className="pcont">
+              Kit; Includes Forward & Rearward Bushing
+            </div>
+            <div className="pfeat">
+              <div className="pfhead">{features[0]}:</div>
+              <div className="pcontent">
+                <ul>
+                  {features.map((data, idx) => {
+                    if (idx == 0 && data.length == 0) return <></>;
+                    return <li>{data}</li>;
+                  })}
+                </ul>
+              </div>
+            </div>
+            <div className="pnums">{props.data.oem}</div>
+            <div className="pabt">
+              Closeout Inventory Warranty Information:Wholesaler Closeout items
+              have a 30 day warranty (but still are covered by our Return
+              Policy).
+            </div>
+            <div className="pprice">$3.20</div>
+          </div>
         </div>
 
-        <div className='pguide'>
-            Buyer’s Guide: QUICK STEER K6395 Control Arm Bushing
+        <div className="pguide">
+          Buyer’s Guide: QUICK STEER K6395 Control Arm Bushing
         </div>
 
-        <div className='pres'>
+        <div className="pres">
           Results may be incomplete due to size of result set
         </div>
 
-        <div className='ptable'>
-          <div className='light'>
-            <div className='pl'>CADILLAC</div>
-            <div className='pm'>ESCALADE</div>
-            <div className='pr'>1999-2000</div>
+        <div className="ptable">
+          <div className="light">
+            <div className="pl">CADILLAC</div>
+            <div className="pm">ESCALADE</div>
+            <div className="pr">1999-2000</div>
           </div>
-          <div className='dark'>
-            <div className='pl'>CADILLAC</div>
-            <div className='pm'>ESCALADE</div>
-            <div className='pr'>1999-2000</div>
+          <div className="dark">
+            <div className="pl">CADILLAC</div>
+            <div className="pm">ESCALADE</div>
+            <div className="pr">1999-2000</div>
           </div>
-          <div className='dark'>
-            <div className='pl'>CADILLAC</div>
-            <div className='pm'>ESCALADE</div>
-            <div className='pr'>1999-2000</div>
+          <div className="dark">
+            <div className="pl">CADILLAC</div>
+            <div className="pm">ESCALADE</div>
+            <div className="pr">1999-2000</div>
           </div>
-          <div className='light'>
-            <div className='pl'>CADILLAC</div>
-            <div className='pm'>ESCALADE</div>
-            <div className='pr'>1999-2000</div>
+          <div className="light">
+            <div className="pl">CADILLAC</div>
+            <div className="pm">ESCALADE</div>
+            <div className="pr">1999-2000</div>
           </div>
-          <div className='dark'>
-            <div className='pl'>CADILLAC</div>
-            <div className='pm'>ESCALADE</div>
-            <div className='pr'>1999-2000</div>
+          <div className="dark">
+            <div className="pl">CADILLAC</div>
+            <div className="pm">ESCALADE</div>
+            <div className="pr">1999-2000</div>
           </div>
-          <div className='light'>
-            <div className='pl'>CADILLAC</div>
-            <div className='pm'>ESCALADE</div>
-            <div className='pr'>1999-2000</div>
+          <div className="light">
+            <div className="pl">CADILLAC</div>
+            <div className="pm">ESCALADE</div>
+            <div className="pr">1999-2000</div>
           </div>
-          <div className='dark'>
-            <div className='pl'>CADILLAC</div>
-            <div className='pm'>ESCALADE</div>
-            <div className='pr'>1999-2000</div>
+          <div className="dark">
+            <div className="pl">CADILLAC</div>
+            <div className="pm">ESCALADE</div>
+            <div className="pr">1999-2000</div>
           </div>
-          <div className='light'>
-            <div className='pl'>CADILLAC</div>
-            <div className='pm'>ESCALADE</div>
-            <div className='pr'>1999-2000</div>
+          <div className="light">
+            <div className="pl">CADILLAC</div>
+            <div className="pm">ESCALADE</div>
+            <div className="pr">1999-2000</div>
           </div>
-          
         </div>
       </Modal.Body>
     </Modal>
@@ -107,42 +120,38 @@ function MyVerticallyCenteredModal(props) {
 }
 
 function Item(props) {
-
-    const [modalShow, setModalShow] = useState(false);
-
+  const [modalShow, setModalShow] = useState(false);
   return (
-    <div className='item-container'>
-    <div onClick={() => setModalShow(true)}>
-    <div className='it'>
-        <div className='ii'>
+    <div className="item-container">
+      <div onClick={() => setModalShow(true)}>
+        <div className="it">
+          <div className="ii">
             <img className="iimage" src={props.image} alt="hub bearing" />
-        </div>
-        <div className='iname'>{props.iname}</div>
-        <div className='ides'>{props.ides}</div>
-        <div className='irat'>
-            <div className='istar'>
-                <div className='istars'>{props.irat}</div>
-                <div className='star'>
-                    <img className="imgstar" src={star} alt="hub bearing" />
-                </div>
+          </div>
+          <div className="iname">{props.iname}</div>
+          <div className="ides">{props.ides}</div>
+          <div className="irat">
+            <div className="istar">
+              <div className="istars">{props.irat}</div>
+              <div className="star">
+                <img className="imgstar" src={star} alt="hub bearing" />
+              </div>
             </div>
-            <div className='inum'>{props.inum}</div>
+            <div className="inum">{props.inum}</div>
+          </div>
+          <div className="iprice">{props.price}</div>
         </div>
-        <div className='iprice'>{props.price}</div>
-
-    </div>
       </div>
-      <div className='modalContainer'>
-      <MyVerticallyCenteredModal
-        setShow={setModalShow}
-        show={modalShow}
-        onHide={() => setModalShow(false)}
-      />
+      <div className="modalContainer">
+        <MyVerticallyCenteredModal
+          setShow={setModalShow}
+          show={modalShow}
+          onHide={() => setModalShow(false)}
+          data={props.data}
+        />
       </div>
-      
-    
     </div>
-  )
+  );
 }
 
-export default Item
+export default Item;
